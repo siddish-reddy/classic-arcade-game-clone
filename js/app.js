1a14babdae82f165dba967ed21e7605de676b955
siddish reddy <Siddish.ibv@gmail.com>
2018-10-13 19:26:14 +0530

    added player class and refactored

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+//rock's dimensions approximately 101x 171, removing shadow
+let X = 101;
+let Y = 83;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -19,18 +23,48 @@ Enemy.prototype.update = function(dt) {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 2 * X;
+    this.y = 4 * 75;
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function(dt) {
+    checkCollisions();
+};
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPressed) {
+    if (keyPressed === 'left') {
+        this.x -= X;
+    } else if (keyPressed === 'right') {
+        this.x += X;
+    } else if (keyPressed === 'up') {
+        this.y -= Y;
+    } else if (keyPressed === 'down') {
+        this.y += Y;
+    }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = new Player();
 
+var allEnemies = [];
+allEnemies.push(new Enemy());
 
+function checkCollisions() {
+    //console.log('cc');
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
